2e37c6efb4d33d8bf41dcd7858623a6ac3f3ca9a
KING BM <killerbm8@gmail.com>
2026-07-28 10:36:42 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+const settings = {
+    botName: "SPOILER-X",
+    ownerName: "King Bm",
+    ownerNumber: "254108487451", // Change to your number
+    prefix: ".",
+    workMode: "public" // Options: "public" or "private"
+};
+
+export default settings;
